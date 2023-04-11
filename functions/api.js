eb1e8146af7f1dd8b0d2f637ec6823c9f03a87d0
syaefulaxl <syaefulaxl@gmail.com>
2023-04-11 13:59:17 +0700

    Fixing For Deployment #1

--- functions/api.js
+++ functions/api.js
@@ -2,7 +2,7 @@ const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const db = require('../db');
+const db = require('./db');
 
 app.use(cors());
 
